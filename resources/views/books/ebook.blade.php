9cf06a0e6ca64215f2ee4f9a71dbb0ccb7fcb387
Saurabh Saxena <saurabh.saxena094@gmail.com>
2018-05-31 22:58:40 +0530

    Editor Added with multiple chapters

--- resources/views/books/ebook.blade.php
+++ resources/views/books/ebook.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.ebook-editor')
 @section('angularjs')
-<link rel="stylesheet" href="http://textangular.com/dist/textAngular.css" type="text/css">
-<script src= "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.js"></script>
-<script src='http://textangular.com/dist/textAngular-rangy.min.js'></script>
-<script src='http://textangular.com/dist/textAngular-sanitize.min.js'></script>
-<script src='http://textangular.com/dist/textAngular.min.js'></script>    
+<link rel="stylesheet" href="/dist/textAngular.css" type="text/css">
+<script src='/dist/angular.js'></script>
+<script src='/dist/textAngular-rangy.min.js'></script>
+<script src='/dist/textAngular-sanitize.min.js'></script>
+<script src='/dist/textAngular.min.js'></script>    
 @endsection
 @section('content')
 	<div layout="column" layout-align="top center">
 		<h1>Editor <span>@{{version}}</span></h1>
-		<div text-angular="text-angular" name="htmlcontent" ng-model="htmlcontent" ta-disabled='disabled'></div>
-		<h1>Raw HTML in a text area</h1>
-		<textarea ng-model="htmlcontent" rows="5" style="width: 100%"></textarea>
+		<div text-angular ng-model="htmlContent" ng-model-options="{ debounce: 500 }" ng-change="updateContent()" name="demo-editor" ta-text-editor-class="clearfix border-around container" ta-html-editor-class="border-around"></div>
+		@{{ chapters | json}}
 	</div>
 @endsection
 @section('footer_scripts')
@@ -29,27 +28,33 @@
 
 		$scope.version = textAngularManager.getVersion();
 		$scope.versionNumber = $scope.version.substring(1);
-		$scope.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE9+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p><h4>Supports non-latin Characters</h4><p>This is text</p>';
-		$scope.htmlcontent = $scope.orightml;
-		$scope.disabled = false;
-		$scope.choices = [{id: 'choice1', name: 'Title'}, {id: 'choice2', name: 'Chapter 1'}];
-   
-		$scope.addNewChapter = function() {
-			var newItemNo = $scope.choices.length+1;
-			$scope.choices.push({'id' : 'choice' + newItemNo, 'name' : 'Chapter ' + newItemNo});
+		$scope.chapters = [];
+		$scope.activeChapterIndex = 0;
+		$scope.counter = 0;
+
+		$scope.viewChapter = function(index) {
+			$scope.htmlContent = $scope.chapters[index].content;
+			$scope.activeChapterIndex = index;
 		};
-		$scope.removeNewChapter = function() {
-			var newItemNo = $scope.choices.length-1;
-			if ( newItemNo !== 0 ) {
-				$scope.choices.pop();
+
+		$scope.addChapter = function() {
+			$scope.counter++;
+			const name = 'Chapter '+ $scope.counter;
+			const content = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE9+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a></p>';
+			$scope.chapters.push({name,content});
+		};
+		$scope.updateContent = function() {
+			const index = $scope.activeChapterIndex;
+			if(!angular.isUndefined($scope.chapters[index])){
+				$scope.chapters[index].content = $scope.htmlContent;    
 			}
 		};
-		$scope.showAddChapter = function(choice) {
-			return choice.id === $scope.choices[$scope.choices.length-1].id;
+		$scope.deleteChapter = function(index) {
+			$scope.chapters.splice(index, 1);
 		};
-		$scope.chapterContent = function(choice){
-			alert(choice.id);
-		};
+
+		$scope.addChapter();
+		$scope.viewChapter(0);
 	}]);
 </script>
 @endsection
