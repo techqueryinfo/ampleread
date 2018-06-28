a25632a6ac866e634a141b9c5c11185a9ad94e91
Saurabh Saxena <saurabh.saxena094@gmail.com>
2018-06-28 15:55:52 +0530

    Editor UI

--- resources/views/books/ebook.blade.php
+++ resources/views/books/ebook.blade.php
@@ -200,7 +200,7 @@
         $scope.addChapter = function() {
             $scope.counter++;
             const name = 'Chapter ' + $scope.counter;
-            const content = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE9+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a></p>';
+            const content = 'Enter Content Here !';
             $scope.chapters.push({
                 name, content
             });
