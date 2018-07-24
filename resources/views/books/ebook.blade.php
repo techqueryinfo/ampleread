c9895e7aed8452552957c0a3b9d5433879417ab8
Saurabh Saxena <saurabh.saxena094@gmail.com>
2018-07-24 21:21:06 +0530

    Editor UI Changes for font & text color

--- resources/views/books/ebook.blade.php
+++ resources/views/books/ebook.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.ebook-editor') @section('angularjs')
-<link rel="stylesheet" href="/dist/textAngular.css" type="text/css">
 <script src='/dist/angular.js'></script>
 <script src='/dist/ng-file-upload-shim.js'></script>
 <script src='/dist/ng-file-upload.js'></script>
 <script src='/dist/textAngular-rangy.min.js'></script>
 <script src='/dist/textAngular-sanitize.min.js'></script>
 <script src='/dist/textAngular.min.js'></script>
+<script src='/dist/angular-spectrum-colorpicker.min.js'></script>
+<script src='/dist/textAngular-dropdownToggle.js'></script>
+<script src='/dist/spectrum.min.js'></script>
+<link rel="stylesheet" href='/dist/textAngular.css' type="text/css"/>
+<link rel="stylesheet" type="text/css" href='/dist/spectrum.min.css'/>
 @endsection @section('content')
 <form action="{{ url('/book/save') }}" method="POST" enctype="multipart/form-data">{{ csrf_field() }}
     <div class="user-bookcreateinfo" ng-cloak>
@@ -328,15 +332,147 @@
 </div>
 @endsection @section('footer_scripts')
 <script type="text/javascript">
-    var app = angular.module('app', ['textAngular', 'ngFileUpload'])
+    var app = angular.module('app', ['textAngular', 'ngFileUpload', 'angularSpectrumColorpicker', 'ui.bootstrap.dropdownToggle'])
     .config(function($provide) {
             // this demonstrates how to register a new tool and add it to the default toolbar
             $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions) { 
+                taRegisterTool('backgroundColor', {
+                    display: "<div spectrum-colorpicker ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></div>",
+                    action: function (color) {
+                        var me = this;
+                        if (!this.$editor().wrapSelection) {
+                            setTimeout(function () {
+                                me.action(color);
+                            }, 100)
+                        } else {
+                            return this.$editor().wrapSelection('backColor', color);
+                        }
+                    },
+                    options: {
+                        replacerClassName: 'fa fa-paint-brush',
+                        showButtons: false
+                    },
+                    color: "#fff"
+                });
+                taRegisterTool('fontColor', {
+                    display: "<spectrum-colorpicker trigger-id='@{{trigger}}' ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></spectrum-colorpicker>",
+                    action: function (color) {
+                        var me = this;
+                        if (!this.$editor().wrapSelection) {
+                            setTimeout(function () {
+                                me.action(color);
+                            }, 100)
+                        } else {
+                            return this.$editor().wrapSelection('foreColor', color);
+                        }
+                    },
+                    options: {
+                        replacerClassName: 'fa fa-font',
+                        showButtons: false
+                    },
+                    color: "#000"
+                });
+                taRegisterTool('fontName', {
+                    display: "<span class='bar-btn-dropdown dropdown'>" + "<button class='btn btn-blue dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-font'></i><i class='fa fa-caret-down'></i></button>" + "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-family: @{{o.css}}; width: 100%' type='button' ng-click='action($event, o.css)'><i ng-if='o.active' class='fa fa-check'></i>@{{o.name}}</button></li></ul></span>",
+                    action: function (event, font) {
+                    //Ask if event is really an event.
+                    if (!!event.stopPropagation) {
+                        //With this, you stop the event of textAngular.
+                        event.stopPropagation();
+                        //Then click in the body to close the dropdown.
+                        $("body").trigger("click");
+                    }
+                    return this.$editor().wrapSelection('fontName', font);
+                },
+                options: [{
+                    name: 'Sans-Serif',
+                    css: 'Arial, Helvetica, sans-serif'
+                }, {
+                    name: 'Serif',
+                    css: "'times new roman', serif"
+                }, {
+                    name: 'Wide',
+                    css: "'arial black', sans-serif"
+                }, {
+                    name: 'Narrow',
+                    css: "'arial narrow', sans-serif"
+                }, {
+                    name: 'Comic Sans MS',
+                    css: "'comic sans ms', sans-serif"
+                }, {
+                    name: 'Courier New',
+                    css: "'courier new', monospace"
+                }, {
+                    name: 'Garamond',
+                    css: 'garamond, serif'
+                }, {
+                    name: 'Georgia',
+                    css: 'georgia, serif'
+                }, {
+                    name: 'Tahoma',
+                    css: 'tahoma, sans-serif'
+                }, {
+                    name: 'Trebuchet MS',
+                    css: "'trebuchet ms', sans-serif"
+                }, {
+                    name: "Helvetica",
+                    css: "'Helvetica Neue', Helvetica, Arial, sans-serif"
+                }, {
+                    name: 'Verdana',
+                    css: 'verdana, sans-serif'
+                }, {
+                    name: 'Proxima Nova',
+                    css: 'proxima_nova_rgregular'
+                }]
+            });
+                taRegisterTool('fontSize', {
+                    display: "<span class='bar-btn-dropdown dropdown'>" + "<button class='btn btn-blue dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-text-height'></i><i class='fa fa-caret-down'></i></button>" + "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-size: @{{o.css}}; width: 100%' type='button' ng-click='action($event, o.value)'><i ng-if='o.active' class='fa fa-check'></i> @{{o.name}}</button></li></ul>" + "</span>",
+                    action: function (event, size) {
+                    //Ask if event is really an event.
+                    if (!!event.stopPropagation) {
+                        //With this, you stop the event of textAngular.
+                        event.stopPropagation();
+                        //Then click in the body to close the dropdown.
+                        $("body").trigger("click");
+                    }
+                    return this.$editor().wrapSelection('fontSize', parseInt(size));
+                },
+                options: [{
+                    name: 'xx-small',
+                    css: 'xx-small',
+                    value: 1
+                }, {
+                    name: 'x-small',
+                    css: 'x-small',
+                    value: 2
+                }, {
+                    name: 'small',
+                    css: 'small',
+                    value: 3
+                }, {
+                    name: 'medium',
+                    css: 'medium',
+                    value: 4
+                }, {
+                    name: 'large',
+                    css: 'large',
+                    value: 5
+                }, {
+                    name: 'x-large',
+                    css: 'x-large',
+                    value: 6
+                }, {
+                    name: 'xx-large',
+                    css: 'xx-large',
+                    value: 7
+                }]
+            });
                 // add the button to the default toolbar definition
                 taOptions.toolbar = [
                     ['bold', 'italics','ul'],
                     ['justifyLeft', 'justifyCenter', 'justifyRight'],
-                    ['insertLink']
+                    ['insertLink'],
+                    ['backgroundColor', 'fontColor', 'fontName', 'fontSize']
                 ];
                 return taOptions;
             }]);
