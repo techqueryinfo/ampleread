efc3c6c5b80da81b3d4a55cff0b7f729162d6c21
vinod <vinod@clecotech.com>
2018-07-24 22:33:42 +0530

    admin stat ui

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.admin')
 @section('content')
+<!-- stat page -->
 <div class="stat-container">
+<!-- stat page one -->
 <div class="stat-one">
     <div class="stat-one-left">
         <div class="row-stat">
@@ -33,8 +35,311 @@
         </div>
     </div>
     <div class="stat-one-right">
+        <div class="chart">
         <div id="chartdiv"></div>
+        </div>
+        <div class="stat-footer">
+            <div class="stat-left">
+                <div class="ratio women">61%</div>
+                <div class="zender">Women</div>
+            </div>
+            <div class="stat-right">
+                <div class="ratio men">39%</div>
+                <div class="zender">Men</div>
+            </div>
+        </div>
     </div>
 </div>
+<!-- stat page two -->
+<div class="stat-two">
+  <div class="stat-two-section">
+      <div class="stat-two-section-head">
+          <div class="head-icon">
+              <i class="fas fa-eye"></i>
+          </div>
+          <div class="head-content">
+              Most viewed books
+          </div>
+      </div>
+      <div class="stat-two-section-body">
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">1. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">2. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">3. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">4. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">5. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">6. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="stat-two-section margin-left-20">
+      <div class="stat-two-section-head">
+          <div class="head-icon">
+              <i class="fas fa-book"></i>
+          </div>
+          <div class="head-content">
+              Most Read books
+          </div>
+      </div>
+      <div class="stat-two-section-body">
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">1. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">2. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">3. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">4. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">5. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">6. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="stat-two-section margin-left-20">
+      <div class="stat-two-section-head">
+          <div class="head-icon">
+              <i class="fa fa-download" aria-hidden="true"></i>
+          </div>
+          <div class="head-content">
+              Most downloaded books
+          </div>
+      </div>
+      <div class="stat-two-section-body">
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">1. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">2. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">3. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">4. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">5. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+          <div class="unit-content">
+              <div class="left">
+                  <div class="one">6. The Ministry of Ut…</div>
+                  <div class="two">Arundhati Roy</div>
+              </div>
+              <div class="right">
+                  <i class="fas fa-eye"></i><span>2,500</span>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
+<!-- stat page three -->
+<div class="stat-three">
+<div class="stat-three-section">
+            <div class="stat-three-section-head">
+                <div class="head-content">
+                    Traffic by devices
+                </div>
+            </div>
+            <div class="stat-three-section-body">
+             <div class="chart">
+                 <div id="xycharts"></div>
+             </div>
+             <div class="content">
+                 <div class="row">
+                     <div class="left">Total visits</div>
+                     <div class="right">112,9 M</div>
+                 </div>
+                 <div class="row">
+                     <div class="left">Avg. visit duration</div>
+                     <div class="right">00:03:43</div>
+                 </div>
+                 <div class="row">
+                     <div class="left">Pages per visit</div>
+                     <div class="right">4.39</div>
+                 </div>
+                 <div class="row">
+                     <div class="left">Bounce rate</div>
+                     <div class="right">52.80%</div>
+                 </div>
+             </div>
+            </div>
+        </div>
+</div>
+<!-- stat page four -->
+<div class="stat-three">
+        <div class="stat-three-section">
+            <div class="stat-three-section-head">
+                <div class="head-content">
+                    Traffic by country
+                </div>
+            </div>
+            <div class="stat-three-section-body">
+                <div class="chart">
+                    <div id="mapchart"></div>
+                </div>
+                <div class="content">
+                    <div class="row">
+                        <div class="left"><img src="../images/flag/india.jpg" alt=""/> INDIA</div>
+                        <div class="right">37.09%
+
+                            <div class="downgrade"><i class="fas fa-angle-down"></i> 0.54%</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="left"><img src="../images/flag/us.jpg" alt=""/> USA</div>
+                        <div class="right">37.09%
+
+                            <div class="downgrade"><i class="fas fa-angle-down"></i> 0.54%</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="left"><img src="../images/flag/canada.jpg" alt=""/> CANADA</div>
+                        <div class="right">37.09%
+
+                            <div class="upgrade"><i class="fas fa-angle-up"></i> 0.54%</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- stat page five -->
+<div class="stat-three">
+        <div class="stat-three-section">
+            <div class="stat-three-section-head">
+                <div class="head-content">
+                    Traffic stats
+                </div>
+            </div>
+            <div class="stat-three-section-body">
+                <div class="chart traficstat">
+                    <div id="traficstat"></div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- end stat page-->
 @endsection
