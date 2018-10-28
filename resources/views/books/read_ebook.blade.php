e3ac1da03f9ada4c4c227efe9b17aa8ecac3799e
vinod <vinod@clecotech.com>
2018-10-28 19:30:14 +0530

    pdf view

--- resources/views/books/read_ebook.blade.php
+++ resources/views/books/read_ebook.blade.php
@@ -76,7 +76,7 @@
     <div class="reader-right">
         @if(!empty($book->book_ext) && $book->book_ext == 'pdf')
         <div class="reader-content" style="margin-left: 0px">
-        <iframe src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/pdfviewer/web/viewer.html?file=/uploads/ebook_logo/{{$book->buyLink}}" style="width: 100%;border-width:0px;" height="700" ></iframe>
+        <iframe src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/pdfviewer/web/viewer.html?file=/uploads/ebook_logo/{{$book->buyLink}}" style="width: 100%;border-width:0px;" height="700" ></iframe>
         </div>
         @else
         <div class="reader-header">
